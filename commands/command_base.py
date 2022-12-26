3d69bb83da237e15490d59cc5b7d95f88f18ed76
yatsuna827 <yatsuna_827@yahoo.co.jp>
2022-12-27 02:45:20 +0900

    環境整備

--- commands/command_base.py
+++ commands/command_base.py
@@ -1,83 +1,83 @@
 from abc import ABC
-from typing import Generator, Sequence, Iterable
 from enum import Enum
+from typing import Iterable
 
 
 class Variables(Enum):
-  VAR_0 = 0
-  VAR_1 = 1
-  VAR_2 = 2
+    VAR_0 = 0
+    VAR_1 = 1
+    VAR_2 = 2
 
 
 class Context:
-  variables: tuple[int, int, int]
+    variables: tuple[int, int, int]
 
-  def __init__(self) -> None:
-    self.variables = [0,0,0]
+    def __init__(self) -> None:
+        self.variables = [0, 0, 0]
 
-  def out(self, value: str):
-    print(value)
+    def out(self, value: str):
+        print(value)
 
 
 class Term:
-  def __init__(self, value: Variables | int) -> None:
-    self._value = value
-    if isinstance(value, int):
-      self._label = str(value)
-    else:
-      self._label = value.name
+    def __init__(self, value: Variables | int) -> None:
+        self._value = value
+        if isinstance(value, int):
+            self._label = str(value)
+        else:
+            self._label = value.name
 
-  @property
-  def label(self) -> str:
-    return self._label
+    @property
+    def label(self) -> str:
+        return self._label
 
-  def resolve(self, context: Context) -> int:
-    if isinstance(self._value, int):
-      return self._value
-    else:
-      return context.variables[self._value.value]
+    def resolve(self, context: Context) -> int:
+        if isinstance(self._value, int):
+            return self._value
+        else:
+            return context.variables[self._value.value]
 
-  def __str__(self) -> str:
-    return self.label
+    def __str__(self) -> str:
+        return self.label
 
 
 class Command(ABC):
-  def exec(self, context: Context) -> None:
-    pass
+    def exec(self, context: Context) -> None:
+        pass
 
 
 class Set(Command):
-  def __init__(self, target: Variables, value: Variables | int) -> None:
-    self._target = target
-    self._value = Term(value)
+    def __init__(self, target: Variables, value: Variables | int) -> None:
+        self._target = target
+        self._value = Term(value)
 
-  def exec(self, context: Context):
-    context.variables[self._target.value] = self._value.resolve(context)
+    def exec(self, context: Context):
+        context.variables[self._target.value] = self._value.resolve(context)
 
 
 class Loop(Command):
-  def __init__(self, times: int, commands: Iterable[Command]) -> None:
-    self._times = times
-    self._commands = commands
-  
-  def exec(self, context: Context) -> None:
-    for _ in range(self._times):
-      for command in self._commands:
-            command.exec(context)
+    def __init__(self, times: int, commands: Iterable[Command]) -> None:
+        self._times = times
+        self._commands = commands
+
+    def exec(self, context: Context) -> None:
+        for _ in range(self._times):
+            for command in self._commands:
+                command.exec(context)
 
 
 class Print(Command):
-  def __init__(self, message: str) -> None:
-    self._message = message
+    def __init__(self, message: str) -> None:
+        self._message = message
 
-  def exec(self, context: Context) -> None:
-    context.out(self._message)
+    def exec(self, context: Context) -> None:
+        context.out(self._message)
 
 
 class CommandStream(Command):
-  def __init__(self, commands: Iterable[Command]) -> None:
-    self._commands = commands
+    def __init__(self, commands: Iterable[Command]) -> None:
+        self._commands = commands
 
-  def exec(self, context: Context = Context()):
-    for command in self._commands:
-      command.exec(context)
+    def exec(self, context: Context = Context()):
+        for command in self._commands:
+            command.exec(context)
