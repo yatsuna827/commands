5980c7478456c3fb2578ab3d76fa4e74292c015c
yatsuna827 <yatsuna_827@yahoo.co.jp>
2022-12-27 02:13:18 +0900

    テストコードに実装意図をすこし込めた

--- tests/test_command.py
+++ tests/test_command.py
@@ -3,19 +3,26 @@ from commands import Variables, Set, CommandStream, Context
 
 def test_set_command():
   ctx = Context()
-  Set(Variables.VAR_0, 10).exec(ctx)
+  command = Set(Variables.VAR_0, 10)
+  assert ctx.variables == [0,0,0]
+  command.exec(ctx)
   assert ctx.variables == [10, 0, 0]
 
-  Set(Variables.VAR_1, Variables.VAR_0).exec(ctx)
+  command = Set(Variables.VAR_1, Variables.VAR_0)
+  command.exec(ctx)
   assert ctx.variables == [10, 10, 0]
 
 
 def test_multiple_commands():
   ctx = Context()
-  CommandStream([
+  command = CommandStream([
     Set(Variables.VAR_0, 10),
     Set(Variables.VAR_1, Variables.VAR_0),
     Set(Variables.VAR_2, 827)
-  ]).exec(ctx)
+  ])
+
+  assert ctx.variables == [0,0,0]
+
+  command.exec(ctx)
 
   assert ctx.variables == [10, 10, 827]
